a28f924cf894bb2004c2ffe20d9aee90a6ba484b
RabbitDrew <Andrei.sp.93@gmail.com>
2023-03-11 23:38:31 +0300

    add: new function

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let  toRedable = number(1)
 console.log (toRedable)
 
 let  number2 =require("num-words")
-let  toRedable2 = number(997)
+let  toRedable2 = number2(997)
 console.log (toRedable2)
+
+function toredable (result) {
+        let number = require ('num-words')
+            result =number(result)
+        console.log(result)   
+}
+toredable((25))
