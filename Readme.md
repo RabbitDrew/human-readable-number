815306b3faa3e6bf3fa63e380ecebd0ca6f9f469
RabbitDrew <Andrei.sp.93@gmail.com>
2023-03-11 22:55:59 +0300

    init: toRedable

---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://github.com/RabbitDrew/human-readable-number.git
---
+++ index.js
@@ -0,0 +1,7 @@
+let  number =require("num-words")
+let  toRedable = number(1)
+console.log (toRedable)
+
+let  number2 =require("num-words")
+let  toRedable2 = number(997)
+console.log (toRedable2)
